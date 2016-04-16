857244b238137dca40f6947d997cbdf0a9edaa9d
Romain Baugue <romain.baugue@elwinar.com>
2016-04-16 19:25:33 +0200

    Implement the main

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	length   uint
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
+
+func init() {
+	flag.UintVar(&length, "l", 6, "length of the tokens")
+}
+
 func main() {
 	println("anvilward, dwarf spy")
+
+	for url := range Resolve(Enumerate(length, alphabet)) {
+		fmt.Println(url)
+	}
 }
